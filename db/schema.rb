4a2aeb79023dd628ebaaaf19084b27f38b8aa267
Nick Blanco <nickblanco@y7mail.com>
2018-11-12 16:50:51 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_181916) do
+ActiveRecord::Schema.define(version: 2018_11_12_190000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boats", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.integer "size"
+    t.integer "capacity"
+    t.integer "price"
+    t.string "captain"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "status", default: "confirmed"
+    t.bigint "boat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_bookings_on_boat_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +47,6 @@ ActiveRecord::Schema.define(version: 2018_11_12_181916) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "bookings", "boats"
 end
