b70b1c5cb91227df08db79682c45ffb606aa5f00
Antonio Griman <griman@gmail.com>
2018-11-14 14:08:32 -0300

    add edit method, add destroy method to boats

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,6 +1,6 @@
 class BoatsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-
+  before_action :set_boat, only: [:edit, :update, :destroy]
   def index
     @boats = Boat.all
   end
@@ -25,9 +25,29 @@ class BoatsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @boat.update(boat_params)
+      redirect_to boat_path(@boat)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @boat.destroy
+    redirect_to root_path
+  end
+
   private
 
   def boat_params
     params.require(:boat).permit(:name, :boat_type, :size, :capacity, :price, :photo) # this is called strong params, for security
+  end
+
+  def set_boat
+    @boat = Boat.find(params[:id])
   end
 end
