b31af59e7571682dfcebfc31a61a2b4b8608fd32
Kayra Czerniewicz <kayracz@gmail.com>
2018-11-14 14:10:36 -0300

    NEW CSS IMPORTANT

---
+++ app/javascript/components/livechat.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+
+
+$("#chat-label").click(function() {
+
+  $('#live-chat').toggleClass('open-chat');
+  $('#chat-label').toggleClass('open-chat-label');
+  $('.live-chat-up-arrow').toggleClass('hide-up-arrow');
+  $('.live-chat-down-arrow').toggleClass('show-down-arrow');
+
+});
