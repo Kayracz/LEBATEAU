5d2dbd24fd326f8a33a823146ec7fcac8f7c0726
Kayra Czerniewicz <kayracz@gmail.com>
2018-11-15 12:32:52 -0300

    Search bar for renters

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,10 +1,14 @@
 class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  BOAT_TYPE = %w(Yatch Sailboat Canoe Launch Houseboat Jetboat Uboat)
+
   has_many :rentee_users, through: :bookings, source: :user
 
   mount_uploader :photo, PhotoUploader
 
   validates :name, :boat_type, :size, :capacity, :price, :photo, presence: true
   validates :name, uniqueness: true
+  validates :boat_type, inclusion: { in: BOAT_TYPE }
 end
