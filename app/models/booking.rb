1142effe010f2255f8ba1a52651aa3d897c1a412
Antonio Griman <griman@gmail.com>
2018-11-15 11:35:12 -0300

    add overlap validation on booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,7 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
   validate :renter_is_not_owner
+  validates :start_date, :end_date, overlap: { scope: "boat_id" }
 
   private
 
